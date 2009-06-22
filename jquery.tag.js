f6dc6f7b09933876bc932dd7eb3516c9a197b813
martin <martin@msaizar.com>
2009-06-22 14:00:50 -0300

    indentation!

--- jquery.tag.js
+++ jquery.tag.js
@@ -7,22 +7,22 @@
 //TODO: make separator configurable
 //TODO: get people and language from dimensions array too 
 
-(function($){  
- jQuery.tagvar = false;
+(function($) {
+    jQuery.tagvar = false;
     $.fn.tagthe = function(options) {   
         var opts = $.extend({},$.fn.tagthe.defaults, options);
         return this.each(function() {
-            $(this).click(function(){
+            $(this).click(function() {
                 text = escape(opts.content);
                 var url = "http://tagthe.net/api/?text="+text+"&view=json&callback=?";
                 if (!jQuery.tagvar) {
-                    
-                    $.getJSON(url, function (data) { 
-                            prnt_html(data,opts); 
-                           jQuery.tagvar = data;
-                        });
-                } else {
-                prnt_html(jQuery.tagvar,opts);
+                    $.getJSON(url, function(data) { 
+                        prnt_html(data,opts); 
+                        jQuery.tagvar = data;
+                    });
+                } 
+                else {
+                    prnt_html(jQuery.tagvar,opts);
                 }
                 console.info(jQuery.tagvar);
             });
@@ -33,14 +33,16 @@
             destination:'#tags',
             content:'Bienvenido al primer tutorial de Javascript y HTML meto a David Beckham para gente.'
     };
-    $.fn.tagthe.format = function (txt) { return txt + " "; }
+    $.fn.tagthe.format = function(txt) { 
+        return txt + " "; 
+    }
     function prnt_html(data,opts) {
         var dim = data.memes[0].dimensions;
         var html_string = '';
         if (dim[opts.info_type] != undefined) {
-            $.each(dim[opts.info_type], function(i,item){
-                    html_string += $.fn.tagthe.format(item);
-                    });     
+            $.each(dim[opts.info_type], function(i,item) {
+                html_string += $.fn.tagthe.format(item);
+            });     
             $(opts.destination).html(html_string);
         }
     }

35802a7df22b0bd2d1e88d0735659c82313253a7
martin <martin@msaizar.com>
2009-06-24 13:43:59 -0300

    added optional word separator, left separator and right separator. now trims last separator from output, and lowercases the string

--- jquery.tag.js
+++ jquery.tag.js
@@ -17,18 +17,23 @@
     $.fn.tagthe.defaults = {
             info_type:'language',
             destination:'#tags',
-            content:''
+            content:'',
+            left_separator:'',
+            right_separator:'',
+            word_separator:', ',
     };
-    $.fn.tagthe.format = function(txt) { 
-        return txt + " "; 
+    $.fn.tagthe.format = function(txt, opts) { 
+        return opts.left_separator + txt + opts.right_separator + opts.word_separator; 
     }
     function prnt_html(data,opts) {
         var dim = data.memes[0].dimensions;
         var html_string = '';
         if (dim[opts.info_type] != undefined) {
             $.each(dim[opts.info_type], function(i,item) {
-                html_string += $.fn.tagthe.format(item);
+                html_string += $.fn.tagthe.format(item, opts);
             });     
+            html_string = html_string.toLowerCase();
+            html_string = html_string.slice(0, 0 - opts.word_separator.length);
             $(opts.destination).html(html_string);
         }
     }
